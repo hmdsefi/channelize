08e43a1a1bb1e703886302425332fe46e1167f87
Hamed Yousefi <hdyousefi@gmail.com>
2022-07-01 11:40:51 +0430

    feature/implement channels, channels registry, and client message validator (#6)
    * implement channels, channels registry, and client message validator
    
    * add unit tests

---
+++ errorx/errors.go
@@ -0,0 +1,64 @@
+/**
+ * Copyright © 2022 Hamed Yousefi <hdyousefi@gmail.com>.
+ */
+
+package errorx
+
+import "fmt"
+
+const (
+	CodeConnectionClosed     = 1000
+	CodeOutboundBufferIsFull = 1001
+
+	CodeFailedToUnmarshalMessage = 1500
+)
+
+const (
+	ErrorMsgConnectionClosed        = "websocket connection is closed"
+	ErrorMsgOutboundBufferIsFull    = "connection outbound buffer is full"
+	ErrorMsgUnmarshalInboundMessage = "failed to unmarshal inbound message"
+	ErrorMsgUnsupportedMessageType  = "message type is not supported"
+	ErrorMsgChannelsIsEmpty         = "channels list is empty, minimum size is 1"
+)
+
+var (
+	code2ErrMsg = map[int]string{
+		CodeConnectionClosed:         ErrorMsgConnectionClosed,
+		CodeOutboundBufferIsFull:     ErrorMsgOutboundBufferIsFull,
+		CodeFailedToUnmarshalMessage: ErrorMsgUnmarshalInboundMessage,
+	}
+)
+
+// ChannelizeError represents a custom error object that holds
+// error details.
+type ChannelizeError struct {
+	Code    int
+	message string
+	err     error
+}
+
+// NewChannelizeError creates a new custom error by using an error Code.
+func NewChannelizeError(code int) *ChannelizeError {
+	return &ChannelizeError{
+		Code:    code,
+		message: code2ErrMsg[code],
+	}
+}
+
+// NewChannelizeErrorWithErr creates a new custom error by wrapping an
+// existing error.
+func NewChannelizeErrorWithErr(code int, err error) *ChannelizeError {
+	return &ChannelizeError{
+		Code:    code,
+		message: code2ErrMsg[code],
+		err:     err,
+	}
+}
+
+func (e ChannelizeError) Error() string {
+	if e.err != nil {
+		return fmt.Sprintf("%s: %s", e.message, e.err)
+	}
+
+	return e.message
+}
---
+++ validation/validatation.go
@@ -0,0 +1,51 @@
+/**
+ * Copyright © 2022 Hamed Yousefi <hdyousefi@gmail.com>.
+ */
+
+package validation
+
+const (
+	FieldType     = "type"
+	FieldChannels = "channels"
+	FieldToken    = "token"
+)
+
+type Validator interface {
+	Validate() *Result
+}
+
+type fieldError struct {
+	Field string `json:"field"`
+	Error string `json:"error"`
+}
+
+func newFieldError(field string, error string) fieldError {
+	return fieldError{Field: field, Error: error}
+}
+
+// Result represent validation result that include error message,
+// error code, and field validation errors.
+type Result struct {
+	Error       string       `json:"error,omitempty"`
+	Code        string       `json:"code,omitempty"`
+	FieldErrors []fieldError `json:"field_errors,omitempty"`
+}
+
+// NewResult creates a new Result object with the input code and error.
+func NewResult(code, err string) *Result {
+	return &Result{
+		Error: err,
+		Code:  code,
+	}
+}
+
+// AddFieldError creates a new fieldError object and adds it to the Result.FieldErrors
+func (r *Result) AddFieldError(field string, err string) {
+	r.FieldErrors = append(r.FieldErrors, newFieldError(field, err))
+}
+
+// IsValid returns false if there is error, code, or field errors does exist.
+// Otherwise, returns true.
+func (r Result) IsValid() bool {
+	return !(r.Error != "" || r.Code != "" || len(r.FieldErrors) != 0)
+}
