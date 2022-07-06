530600a3159aaab56ddfd8cc4a1924330208da3c
Hamed Yousefi <hdyousefi@gmail.com>
2022-07-06 16:16:02 +0430

    implement public channels example, create readme for the example app

---
+++ _examples/public-channels/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/websocket"
+	"github.com/hamed-yousefi/channelize"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	chlz := channelize.NewChannelize()
+
+	newsChannel := channelize.RegisterPublicChannel("news")
+	go publish(ctx, chlz, newNews, newsChannel)
+
+	alertsChannel := channelize.RegisterPublicChannel("alerts")
+	go publish(ctx, chlz, newAlert, alertsChannel)
+
+	notificationChannel := channelize.RegisterPublicChannel("notifications")
+	go publish(ctx, chlz, newNotification, notificationChannel)
+
+	go initiateAndServe(ctx, chlz)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	cancel()
+}
+
+func initiateAndServe(ctx context.Context, chlz *channelize.Channelize) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	http.HandleFunc("/ws", chlz.MakeHTTPHandler(ctx, upgrader))
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,23 +4,13 @@ go 1.17
 
 require (
 	github.com/gorilla/websocket v1.5.0
-	github.com/labstack/echo/v4 v4.7.2
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.7.5
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/labstack/gommon v0.3.1 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
-	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
